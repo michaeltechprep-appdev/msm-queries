577dcecaefd12e003ab206be1657388f870654e8
michael_techprep-appdev <Miketj1130@yahoo.com>
2023-03-25 15:11:06 +0000

    Latest Version

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get("directors/youngest", { :controller => "directors", :action => "newest"})
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+
 end
