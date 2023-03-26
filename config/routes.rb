5b6616307c3c126fa8c3daa84fc45c025d582477
michael_techprep-appdev <Miketj1130@yahoo.com>
2023-03-26 05:17:14 +0000

    Best so far

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
 
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/movies/:an_id", { :controller => "movies", :action => "movie_details" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
+
+  get("/actors/:an_id", { :controller => "actors", :action => "actor_details" })
 end
